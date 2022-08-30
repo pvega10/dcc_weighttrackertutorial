fc8ce43e50ffb067984769f844167de6e36db40b
Paul M Vega <Paul@macbook-pro-2.lan>
2022-08-29 19:33:14 -0500

    completed entry chart tracker component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 
 function App() {
@@ -18,6 +19,7 @@ function addNewEntry (entry){
 
      <DisplayEntries parentEntries = {entries} />
      <AddEntryForm addNewEntryProperty ={addNewEntry}/>
+     <EntriesChartTracker parentEntries ={ entries}/>
 
     </div>
   );
